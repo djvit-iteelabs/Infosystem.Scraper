b6e2e7f68aba1052e7fd78958daa434d78083d8b
djvit-iteelabs <vitaly@iteelabs.com>
2011-02-14 08:34:19 +0200

    1. Initial project commit 2. Code by tarik 3. Qartz based service

---
+++ JobAssembly/SuperJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz;
+
+using Common.Logging;
+
+
+namespace JobAssembly
+{
+    public class SuperJob: IJob
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(SuperJob));
+
+        #region IJob Members
+
+        public void Execute(JobExecutionContext context)
+        {
+
+            logger.Info("SuperJob is launched");
+        }
+
+        #endregion
+    }
+}
